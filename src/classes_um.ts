5cfc8344a75ce86d8538d3145c3177446db10c7f
Judson Paiva <judson.paiva@hotmail.com>
2026-02-23 21:57:41 +0100

    estudo de classes

--- src/classes_um.ts
+++ src/classes_um.ts
@@ -8,9 +8,19 @@ class Loja {
         this.categoria = categoria;
     }
 
+    criarLoja(): void {
+        console.log(`Loja ${this.nome} criada com sucesso`);
+    }
+
+    emitirPedidos(mesa: number): string {
+        return `Pedido emitido com sucesso para a mesa ${mesa}`;
+    }
+
 }
 
 
 const burguerKing = new Loja("Burguer King", "Fast Food");
-console.log(burguerKing.nome);
-console.log(burguerKing.categoria);
+burguerKing.criarLoja();
+console.log(burguerKing.emitirPedidos(1));
+// console.log(burguerKing.nome);
+// console.log(burguerKing.categoria);
